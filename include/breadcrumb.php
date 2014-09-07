1fdf5bd689cb7b850c7bfc80a77e2c21afa2c291
Han <HanJiang.it@gmail.com>
2014-09-07 15:16:04 +0800

    breadcrumb refine

--- include/breadcrumb.php
+++ include/breadcrumb.php
@@ -4,9 +4,8 @@
 	  <li> <a href="<?=site_url();?>">Home</a> </li>
 	  <li class="active"><a href="#"><?php single_cat_title(); ?></a> </li>
 	  <div class="single-js-change-font-size pull-right">
-				<a href="javascript:doZoom(14)">小</a> 
-				<a href="javascript:doZoom(16)">中</a> 
-				<a href="javascript:doZoom(20)">大</a>
+				<a href="javascript:doZoom(16)"><small>A</small></a>  
+				<a href="javascript:doZoom(20)">A</a>
 		</div>
 	</ul>
 </div>
